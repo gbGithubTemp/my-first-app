7d9ad0e7baf3d1118c1589072eb5f84947a5fa81
Gaurang <gaurang.niksantech@gmail.com>
2024-06-26 16:54:28 +0530

    Updated

--- Frontend/src/app/Property/property-list/property-list.component.ts
+++ Frontend/src/app/Property/property-list/property-list.component.ts
@@ -1,10 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-property-list',
   templateUrl: './property-list.component.html',
   styleUrls: ['./property-list.component.css']
 })
-export class PropertyListComponent {
+export class PropertyListComponent implements OnInit {
+
+  properties : Array<any> = [
+    {
+      "Id" : 1,
+      "Name" : "Birla House",
+      "Type" : "House",
+      "Price" : 12000
+    },
+    {
+      "Id" : 2,
+      "Name" : "Sanidhya House",
+      "Type" : "House",
+      "Price" : 14000
+    },
+    {
+      "Id" : 3,
+      "Name" : "PushpKunj House",
+      "Type" : "House",
+      "Price" : 10000
+    },
+    {
+      "Id" : 4,
+      "Name" : "Hiradhan House",
+      "Type" : "House",
+      "Price" : 16000
+    },
+    {
+      "Id" : 5,
+      "Name" : "Kudrat House",
+      "Type" : "House",
+      "Price" : 19000
+    },
+    {
+      "Id" : 6,
+      "Name" : "Macro House",
+      "Type" : "House",
+      "Price" : 21000
+    }
+    
+  ]
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  
 
 }
